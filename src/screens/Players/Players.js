e2c3e412a1f35fe4f2ef102a3e069efd2e2e38c5
Adam Schlichtmann <aschlichtmann@dmsi.com>
2019-09-11 17:43:53 -0500

    Working search function for players

--- src/screens/Players/Players.js
+++ src/screens/Players/Players.js
@@ -1,15 +1,52 @@
 import React, { useStyles } from "react";
-import { View } from "react-native";
+import { View, Text, TextInput, FlatList } from "react-native";
 import PropTypes from "prop-types";
 
 import PlayersStyles from "./Players.styles";
 
 const Players = props => {
-  return <View style={PlayersStyles.pageContainer}></View>;
+  return (
+    <View style={PlayersStyles.pageContainer}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          margin: 10
+        }}
+      >
+        <Text style={{ margin: 10 }}>Enter Player:</Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            flex: 1,
+            margin: 10,
+            padding: 5
+          }}
+          onChangeText={text => props.filterPlayers(text)}
+          value={props.searchText}
+          placeholder={"Enter Player"}
+        ></TextInput>
+      </View>
+      <View>
+        {props.players.map(player => (
+          <View>
+            <Text>{player.player.fullName}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
 };
 
 Players.propTypes = {
-  style: PropTypes.oneOfType([PropTypes.object, PropTypes.func])
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+  filterPlayers: PropTypes.func.isRequired,
+  searchText: PropTypes.string.isRequired,
+  players: PropTypes.arrayOf(PropTypes.object).isRequired,
+  allPlayers: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 Players.defaultProps = {
--- src/screens/Players/Players.wrapper.js
+++ src/screens/Players/Players.wrapper.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
+import { Client } from "espn-fantasy-football-api";
 import Players from "./Players";
 
 class PlayersWrapper extends Component {
@@ -16,11 +16,64 @@ class PlayersWrapper extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      searchText: "",
+      allPlayers: [],
+      players: []
+    };
   }
 
+  componentDidMount() {
+    const s2 =
+      "AEANKvL8zOeia%2FZT9HW32kIKY4gsh%2B4w08%2BlCtpRBiVCfNipvZL5tVtpEzmXwe9zV6DpEm8gNq8ecYETodcz4zyiOgxKz%2FGjMoawtHKcM3eqSwoN9z2UErSc4jMihAwQ632vPJIznjDZRedyWx%2B8UZB3gffPkiLDa1nep1Hk3alczSAwMZ63odbqtFwVXyyVwEQtigZt7CzxzXE8ErgJ2EPiSUwoE%2BQFF3%2B6Uh6pDSIjkb8lzbJjqdksi2mlL4%2BeNVqxltUjk575E5np0hLty9joU6%2BS2ygvON1sG6AaOrrsfQ%3D%3D";
+    const SWID = "29C3AF40-B731-496E-BB0D-1E545011C530";
+    const dynasty = this.state.leagueID;
+
+    const myClient = new Client({ leagueId: 286277 });
+    myClient.setCookies({
+      espnS2: s2,
+      SWID: SWID
+    });
+
+    myClient
+      .getTeamsAtWeek({ seasonId: 2019, scoringPeriodId: 1 })
+      .then(team => {
+        const players = [...this.state.players, ...team.roster];
+        this.setState({ allPlayers: players });
+      });
+    myClient
+      .getFreeAgents({ seasonId: 2019, scoringPeriodId: 1 })
+      .then(player => {
+        const players = [...this.state.players, ...player];
+        this.setState({ allPlayers: players });
+      });
+  }
+
+  filterPlayers = searchText => {
+    this.setState({ searchText }, () => {
+      if (this.state.searchText !== "") {
+        const players = this.state.allPlayers;
+        const searchResults = players.filter(player =>
+          player.player.fullName
+            .toUpperCase()
+            .includes(this.state.searchText.toUpperCase())
+        );
+        this.setState({ players: searchResults });
+      }
+    });
+  };
+
+  onChangeText = text => this.setState({ searchText: text });
+
   render() {
-    return <Players />;
+    return (
+      <Players
+        filterPlayers={this.filterPlayers}
+        searchText={this.state.searchText}
+        players={this.state.players}
+        allPlayers={this.state.allPlayers}
+      />
+    );
   }
 }
 
